c44dccfc05aea509374c27769644086f85579d2e
“bgiorgi1” <brianna.giorgi@gmail.com>
2021-05-19 10:57:01 -0400

    index page created

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,26 +1,37 @@
-const express=require('express');
-const router=express.Router();
-const passport = require('../config/ppConfig');
-const db = require('../models') //bring in database
+const express = require("express");
+const router = express.Router();
+const passport = require("../config/ppConfig");
+const db = require("../models"); //bring in database
 
-router.get('/', async (req,res) => {
-    //grab all reviews from database
-    const fetchReviews=await db.review.findall()
-    res.render('reviews/index', {review: fetchReviews});
-    })
+// router.get("/", async (req, res) => {
+//   //grab all reviews from database
+//   const fetchReviews = await db.review.findall();
+//   res.render("reviews/index", { review: fetchReviews });
+// });
 
-router.get('/new', (req,res) =>{
-    res.render('reviews/new');
-  })
-  
-  router.post('/', async (req, res) => {
-    const{title, description}=req.body;
-    console.log(title, description);
-  
-    const newReview = await db.reviews.create({title, description});
-    console.log(newReview);
-    res.redirect('/reviews')
-  
-  })
+router.get("/", (req, res) => {
+  db.reviews
+    .findAll()
 
-module.exports=router;
+    .then((resultReviews) => {
+      // console.log("here is fav park");
+      // console.log(favPark);
+      res.render("reviews/index", { resultReviews });
+    });
+});
+
+router.get("/new", (req, res) => {
+  res.render("reviews/new");
+});
+
+router.post("/", async (req, res) => {
+  const { name, description } = req.body;
+  console.log(name, description);
+
+  const newReview = await db.reviews.create({ name, description });
+  console.log(newReview);
+  res.redirect("/reviews");
+});
+
+module.exports = router;
+
